d8d14a8e49934e07a55f655fb69f43767c977416
Rosalind Skinner <ros.skinner@gmail.com>
2014-08-27 13:17:06 +1000

    svg scroll animate

--- javascripts/svg.js
+++ javascripts/svg.js
@@ -2,22 +2,28 @@
 $(document).ready(function(){
 
   $(window).scroll(function() {
-      drawLine( $('#route'),
-                document.getElementById('path') );
+      drawLine( $('.route'),
+                $('.path')[0] );
+      drawLine( $('.route'),
+                $('.path')[1] );
     });
 
     // init the line length
-      drawLine( $('#route'),
-                document.getElementById('path') );
+      drawLine( $('.route'),
+                $('.path')[0] );
+      drawLine( $('.route'),
+                $('.path')[1] );
 
     //draw the line
+
+
     function drawLine(container, line){
 
       var pathLength = line.getTotalLength(),
           maxScrollTop = $(document).height() - $(window).height(),
           percentDone = $(window).scrollTop() / maxScrollTop,
           length = percentDone * pathLength;
-      line.style.strokeDasharray = [ length ,pathLength].join(' ');
+      line.style.strokeDasharray = [ 15 *length ,pathLength].join(' ');
     }
 
 });
